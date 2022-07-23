593a70abfc9fe5f563689fd4666709f05a221855
Nazmul Hoda <cse.nazmul.bubt@gmail.com>
2022-07-23 14:53:50 +0600

    [Edit] updated share functionality

---
+++ ERSocialShareManager/AppManager.swift
@@ -0,0 +1,13 @@
+//
+//  AppManager.swift
+//  ERSocialShareManager
+//
+//  Created by Nazmul on 23/07/2022.
+//
+
+import UIKit
+
+class AppManager: NSObject {
+    static let shared = AppManager()
+    var lastSavedObjectPhassetIndentifier = ""
+}
